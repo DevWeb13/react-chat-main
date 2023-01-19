70657db5fa794ac7a0c8bd1f301df7690b26eaaa
DevWeb13 <giuliano.loic@sfr.fr>
2023-01-19 08:29:00 +0100

    fix: (App.js): 'useState' is defined but never used.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import NavBar from "./components/NavBar";
 import ChatBox from "./components/ChatBox";
 import Welcome from "./components/Welcome";
-import { useState } from "react";
 
 function App() {
   const [user] = useAuthState(auth);
