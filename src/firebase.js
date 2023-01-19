3406238a17f3fd755057624789f2144a9c501977
DevWeb13 <giuliano.loic@sfr.fr>
2023-01-19 07:21:28 +0100

    feat: (src/components/firebase.js): Authenticate Users with Their Google Account
    Delete 'dotenv': React has its own dotenv adding REACT_APP_<variable name>

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,3 @@
-import 'dotenv/config'
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
@@ -7,6 +5,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
